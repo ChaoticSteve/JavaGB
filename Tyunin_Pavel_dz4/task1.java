e4ad3d1d75b3ef71fa50d98e192388a60dd70af9
Tyunin Pavel <tuninpavel@gmail.com>
2023-03-14 22:14:47 +0600

    task 4 done

---
+++ Tyunin_Pavel_dz4/task1.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+/**Пусть дан LinkedList с несколькими элементами. 
+Реализуйте метод, который вернет “перевернутый” список.*/
+class Task4_1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        Random rand = new Random();
+        for (int i = 0; i < 20; i++) {
+            originalList.add(i, rand.nextInt(100));
+        }
+        System.out.printf("Исходный список: %s\n", originalList);
+
+        System.out.printf("Перевернутый список: %s\n", getReverse(list));
+
+    }
+
+    public static LinkedList<Integer> getReverse(LinkedList<Integer> list) {
+        LinkedList<Integer> reversed = new LinkedList<>();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            reversed.add(list.get(i));
+        }
+        return reversed;
+    }
+}
